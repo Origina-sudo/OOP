16668b971a485ebb5f94cb18a53934aa6e9fb3ca
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-16 00:16:19 +0930

    h

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 class Item():
-    pass
-
+    def calculator_total_price(self,x,y):
+        return x * y
 
 item1 = Item()
 item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
+print(item1.calculator_total_price(item1.price ,item1.quantity))
 
-random_str = "aaa"
-
-print(random_str.upper())
+item2 = Item()
+item2.name = "Laptop"
+item2.price = 1000
+item2.quantity = 3
+print(item2.calculator_total_price(item2.price ,item2.quantity))
