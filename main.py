f3d6033c1fa064ab087ee1745aa27475d9e42ebb
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-16 00:28:29 +0930

    Changes

--- main.py
+++ main.py
@@ -1,4 +1,9 @@
 class Item():
+    def __init__(self):
+        print("I am created !")
+
+
+
     def calculator_total_price(self,x,y):
         return x * y
 
@@ -6,10 +11,9 @@ item1 = Item()
 item1.name = "Phone"
 item1.price = 100
 item1.quantity = 5
-print(item1.calculator_total_price(item1.price ,item1.quantity))
+
 
 item2 = Item()
 item2.name = "Laptop"
 item2.price = 1000
 item2.quantity = 3
-print(item2.calculator_total_price(item2.price ,item2.quantity))
