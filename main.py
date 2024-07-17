435ed4559f6cc6c4eb50cf181d0b17b36c2499c1
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-17 23:28:29 +0930

    Hello

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
 class Item():
-    def __init__(self,name,price,quantity ):
+    def __init__(self,name:str,price:float,quantity =0 ):
+        #Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal toZero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to  Zero!"
+
+        #Assign to Self Object
         self.name = name
         self.price = price
         self.quantity =quantity
-    def calculator_total_price(self,x,y):
-        return x * y
 
-item1 = Item("Phone",100,5)
-item1.price = 100
-item1.quantity = 5
+    def calculator_total_price(self):
+        return self.price * self.quantity
 
-item2 = Item("Laptop",1000,3)
-item2.price = 1000
-item2.quantity = 3
+item1 = Item("Phone",100,1)
+#item1.price = 100
+#item1.quantity = 5
 
-print(item2.name)
-print(item1.name)
-print(item1.price)
-print(item2.price)
-print(item2.quantity)
+item2 = Item("Laptop",1000 ,3)
+#item2.price = 1000
+#item2.quantity = 3
+
+print(item1.calculator_total_price())
+print(item2.calculator_total_price())
