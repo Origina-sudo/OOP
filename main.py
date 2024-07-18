2eae140e108037f9706ca03ee2d23b9b0ce3bc5c
Benjamin Arthur <arthurba8585@gmail.com>
2024-07-18 23:12:32 +0930

    changes

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-class Item():
+import csv
+
+
+class Item:
     pay_rate = 0.8 # The pay rate after 20% discount
     all = []
-    def __init__(self,name:str,price:float,quantity =0 ):
-        #Run validations to the received arguments
-        assert price >= 0, f"Price {price} is not greater than or equal toZero!"
-        assert quantity >= 0, f"Quantity {quantity} is not greater than or equal to  Zero!"
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Price {price} is not greater than or equal to zero!"
+        assert quantity >= 0, f"Quantity {quantity} is not greater or equal to zero!"
 
-        #Assign to Self Object
+        # Assign to self object
         self.name = name
         self.price = price
         self.quantity = quantity
@@ -14,36 +17,36 @@ class Item():
         # Actions to execute
         Item.all.append(self)
 
-    def calculator_total_price(self):
+    def calculate_total_price(self):
         return self.price * self.quantity
 
     def apply_discount(self):
         self.price = self.price * self.pay_rate
 
+    @classmethod
+    def instantiate_from_csv(cls):
+        with open('items.csv', 'r') as f:
+            reader = csv.DictReader(f)
+            items = list(reader)
+
+        for item in items:
+            Item(
+                name=item.get('name'),
+                price=float(item.get('price')),
+                quantity=int(item.get('quantity')),
+            )
+
+    @staticmethod
+    def is_integer(num):
+        # We will count out the floats that are point zero
+        # For i.e: 5.0, 10.0
+        if isinstance(num, float):
+            # Count out the floats that are point zero
+            return num.is_integer()
+        elif isinstance(num, int):
+            return True
+        else:
+            return False
+
     def __repr__(self):
-        return f"Item('{self.name}',{self.price},{self.quantity})"
-
-
-
-"""item1 = Item("Phone",100,1)
-#item1.price = 100
-#item1.quantity = 5
-
-item2 = Item("Laptop",1000 ,3)
-item2.pay_rate =0.7
-item2.apply_discount()
-print(item2.price)
-#item2.price = 1000
-#item2.quantity = 3
-
-item1.apply_discount()
-print(item1.price)"""
-
-item1 = Item("Phone",100,1)
-item2 = Item("Laptop",1000,3)
-item3 = Item("Cable",10,5)
-item4 = Item("Mouse",50,5)
-item5 = Item("Keyboard",75,5)
-
-
-print(Item.all)
+        return f"Item('{self.name}', {self.price}, {self.quantity})"
